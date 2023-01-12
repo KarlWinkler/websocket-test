735f217275cea7f90b7c97ca9ef473536f8cadbb
karlWinkler <karlw200@gmail.com>
2023-01-11 20:45:33 -0700

    boilerplate channel setup

---
+++ backend/websockets/messaging/consumers.py
@@ -0,0 +1,34 @@
+import asyncio
+import json
+from channels.consumer import AsyncConsumer
+
+class ChatConsumer(AsyncConsumer):
+  async def websocket_connect(self, event):
+    print('connected', event)
+    chat = "chat room"
+    self.chat = chat
+    await self.channel_layer.group_add(
+      chat,
+      self.channel_name
+    )
+    await self.send({
+      "type": "websocket.accept"
+    })
+
+  async def websocket_receive(self, event):
+    drawing_data = event.get('text', None)
+    await self.channel_layer.group_send(
+      self.board_room,
+      {
+        "type": "board_message",
+        "text": drawing_data
+    })
+
+  async def board_message(self, event):
+    await self.send({
+      "type": 'websocket.send',
+      'text': event['text']
+    })
+
+  async def websocket_disconnect(self, event):
+    print('disconnected', event)
